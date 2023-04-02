f08f646bb6818805be687d36f8801d145c3cf43e
Muhammad Haroon Zafar <mr.haroon.zafar@gmail.com>
2023-04-02 15:24:22 +0500

    Added Sessions 06, 07

---
+++ textpreprocessing.py
@@ -0,0 +1,63 @@
+
+# print("Module execurtedddd......")
+# def orderPizza(size, *toppings):
+#     '''
+#     This function takes int value of size
+#     Size: S, M , L EXL
+#     and an arbitarary argument Toppings 
+#     Toppings: a,b,c,d,f,g,r,t,y,u
+#     This finction returns your ordered pizza 
+#     '''
+#     toppings_avialable = ['mushrrom', 'olive','chhesse', 'garlic','capchica']
+#     applied_toppings=[]
+#     for top in toppings:
+#         if top not in toppings_avialable:
+#             print(f"{top} is not availale")
+#         else:
+#             applied_toppings.append(top)
+            
+#     return f"Your {size} inches pizza is ready with {applied_toppings}"
+    
+
+# def build_profile(first, last, **user_info):
+#     ''' 
+       
+#     this function takes a string value of name , last name and keyword arguments for profile other info
+    
+#     first : str
+#     last : str 
+#     user info : keyword aguments
+    
+#     '''
+
+#     profile = {}
+#     profile['first_name'] = first
+#     profile['last_name'] = last
+#     profile.update(user_info)
+#     return profile
+    
+
+deef add():
+    print(add)
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
